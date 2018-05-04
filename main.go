96900c0c2fcf2540e26e260795679f5787c60882
boeing888 <735369269@qq.com>
2018-05-04 15:21:01 +0800

    structure fix

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"gin_demo/utils"
+	"github.com/SBOrg666/lite-yun-golang/utils"
 	"github.com/gin-contrib/sessions"
 	"github.com/satori/go.uuid"
 	"github.com/jasonlvhit/gocron"
